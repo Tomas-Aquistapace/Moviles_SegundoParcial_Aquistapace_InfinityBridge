70bd44d190d7ea38e2025b545c70bb1c532a0911
Tomas <Aquista35@gmail.com>
2021-10-21 21:28:35 -0300

    Added different tutorials

---
+++ Moviles Segundo Parcial - The Infinity Bridge/Assets/Scripts/UI/UI_LoadingTutorial.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_LoadingTutorial : MonoBehaviour
+{
+    [Header("Fingers: ")]
+    [SerializeField] private GameObject[] fingers = new GameObject[3];
+
+    [Header("Sprites: ")]
+    [SerializeField] private Sprite fingerImg;
+    [SerializeField] private Sprite[] mouseImg;
+
+    [Header("Texts: ")]
+    [SerializeField] TextMeshProUGUI moveTutorial;    
+    [SerializeField] TextMeshProUGUI rotateTutorial;
+
+    [SerializeField] [TextArea] private string mobileMoveText;
+    [SerializeField] [TextArea] private string mobileRotateText;
+
+    [SerializeField] [TextArea] private string mobilePcText;
+    [SerializeField] [TextArea] private string rotatePcText;
+
+    private void Awake()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+
+        moveTutorial.text = mobilePcText;
+        rotateTutorial.text = rotatePcText;
+
+        for (int i = 0; i < fingers.Length; i++)
+        {
+            fingers[i].GetComponent<Image>().sprite = mouseImg[i];
+        }
+
+#elif UNITY_ANDROID || UNITY_IOS
+        
+        moveTutorial.text = mobileMoveText;
+        rotateTutorial.text = mobileRotateText;
+
+        for (int i = 0; i < fingers.Length; i++)
+        {
+            fingers[i].GetComponent<Image>().sprite = fingerImg;
+        }
+        
+#endif
+    }
+}
